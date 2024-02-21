6b23622944615cc97f7ebbf4d61a9248737cd95f
mazax1ct <vova@giveandget.ru>
2024-02-21 21:29:04 +0300

    some pages

--- src/js/main.js
+++ src/js/main.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
     });
   }
 
-  /*if($('.js-clients').length) {
+  if($('.js-clients').length) {
     $('.js-clients').slick({
       infinite: true,
       slidesToShow: 3,
@@ -44,10 +44,39 @@ $(document).ready(function() {
         }
       ]
     });
-  }*/
+  }
+
+  if($('.js-short-about-slider').length) {
+    $('.js-short-about-slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: true
+    });
+  }
+
+  if($('.js-features_2-slider').length) {
+    $('.js-features_2-slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      prevArrow: '<button class="slick-arrow slick-prev" aria-label="Назад" type="button"><svg aria-hidden="true"><use xlink:href="images/sprite.svg#arrow_right"/></svg></button>',
+      nextArrow: '<button class="slick-arrow slick-next" aria-label="Вперед" type="button"><svg aria-hidden="true"><use xlink:href="images/sprite.svg#arrow_right"/></svg></button>',
+      mobileFirst: true,
+      responsive: [
+        {
+          breakpoint: 1023,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+          }
+        }
+      ]
+    });
+  }
 });
 
-//открытие мобильного меню меню
+//открытие мобильного меню
 $(document).on('click', '.js-mobile-menu-opener', function () {
   $('.mobile-menu').fadeIn(function () {
     $('.mobile-menu__inner').addClass('is-active');
@@ -57,7 +86,7 @@ $(document).on('click', '.js-mobile-menu-opener', function () {
   return false;
 });
 
-//закрытие мобильного меню меню
+//закрытие мобильного меню
 $(document).on('click', '.js-mobile-menu-closer', function () {
   $('.mobile-menu__inner').removeClass('is-active');
   $('.mobile-menu').fadeOut();
@@ -74,14 +103,14 @@ function closeMobileMenu(evt) {
 	}
 }
 
-//открытие мобильного меню
+//тогглер основного меню
 $(document).on('click', '.js-menu-toggler', function() {
   $(this).toggleClass('is-active');
   $('.menu__container').slideToggle();
   return false;
 });
 
-//мобильное меню
+//основное меню на мобилах
 $(document).on('click', '.js-menu-button', function () {
   if(!$(this).hasClass('is-active')){
     $('.menu__lvl-block').hide();
@@ -108,6 +137,7 @@ $(document).on('click', '.js-menu-button', function () {
   return false;
 });
 
+//основное меню на десктопах
 if($('body').width() > 1199) {
   $(document).on('mouseover', '.menu__item', function() {
     var link = $(this).find('.menu__button').attr('data-target');
@@ -129,5 +159,14 @@ if($('body').width() > 1199) {
 //закрытие fancybox
 $(document).on('click', '.js-popup-closer', function () {
   $.fancybox.close();
+  return false;
+});
+
+//табы в контактах
+$(document).on('click', '.js-contacts', function () {
+  $('.js-contacts').removeClass('is-active');
+  $(this).addClass('is-active');
+  $('.contacts__tab').removeClass('is-active');
+  $('.contacts__tab[data-target="'+ $(this).attr('data-target') +'"]').addClass('is-active');
   return false;
 });
